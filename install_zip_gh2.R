7ec3b2772200f2fda2b3ced115d1d6429ad523ee
AuPtZ <5180203+AuPtZ@users.noreply.github.com>
2022-06-24 18:30:07 +0800

    Add files via upload
    main code

---
+++ install_zip_gh2.R
@@ -0,0 +1,17 @@
+if (!require("yulab.utils", character.only = TRUE)) {
+  install.packages(x, dependencies = TRUE)
+  library(x, character.only = TRUE)
+}
+
+install_zip_gh2 <- function(proxy_url='https://ghproxy.com/https://github.com/', repo, ref = "master", args = "--no-build-vignettes") {
+  url <- paste0(proxy_url, repo, '/archive/refs/heads/', ref, '.zip')
+  f <- tempfile(fileext=".zip")
+  method <- "auto"
+  if (.Platform$OS.type == "windows") method <- "curl"
+  utils::download.file(url, destfile=f, method = method)
+  install_zip(f, args=args)
+}
+
+# Demo code
+# Not run
+# install_zip_gh2("BioinformaticsFMRP/TCGAbiolinks")
