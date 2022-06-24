d88006fda6d2e7878da60e7cc895c4923f163f0c
AuPtZ <5180203+AuPtZ@users.noreply.github.com>
2022-06-24 10:34:12 +0000

    Update install_zip_gh2.R

--- install_zip_gh2.R
+++ install_zip_gh2.R
@@ -3,7 +3,7 @@ if (!require("yulab.utils", character.only = TRUE)) {
   library(x, character.only = TRUE)
 }
 
-install_zip_gh2 <- function(proxy_url='https://ghproxy.com/https://github.com/', repo, ref = "master", args = "--no-build-vignettes") {
+install_zip_gh2 <- function(repo, ref = "master", proxy_url='https://ghproxy.com/https://github.com/',  args = "--no-build-vignettes") {
   url <- paste0(proxy_url, repo, '/archive/refs/heads/', ref, '.zip')
   f <- tempfile(fileext=".zip")
   method <- "auto"
